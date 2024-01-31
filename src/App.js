f2cdd544cee232d4bc820b0bd7d756198051f9bb
Aniket-kr1030 <88719269+Aniket-kr1030@users.noreply.github.com>
2024-01-31 21:11:00 +0530

    small changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HeroSection from './components/HeroSection/HeroSection';
+import HeroSection from './components/Home/HeroSection/HeroSection';
 import SeaconvoyDesc from './components/Home/SeaconvoyDesc/SeaconvoyDesc';
 import CustomersScroller from './components/Home/CustomersScroller/CustomersScroller';
 import ProductAndServices from './components/Home/ProductAndServices/ProductAndServices';
