f15852a10061bc1cc9490c5f2dc9318f503e0bbf
Aniket-kr1030 <88719269+Aniket-kr1030@users.noreply.github.com>
2024-01-31 21:08:35 +0530

    hero section pasted to home

--- src/components/Home/ProductAndServices/ProductAndServices.js
+++ src/components/Home/ProductAndServices/ProductAndServices.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './product-and-services.css';
+import Card from '../../Shared/Card'; 
+import CardsContainer from '../../Shared/CardsContainer'; 
 
 function ProductAndServices() {
   return (
     <div className="product-and-services-container">
-      {/* Content goes here */}
+
+    <img src="./images/home-page/prod-serv/prod-serv-heading.svg" alt="" width="1000px"/>
+
+    <CardsContainer>
+      <Card
+      bgImage='././images/home-page/prod-serv/prod-improv-heading.svg'>
+        <p><span>Design and indenisation</span> of waterjet propulsion system, electric popped propulsion system,
+          hydraulic steering gear system for marine vehicles such as ships, amphibious vehicles, underwater vehicles.
+        </p>
+      </Card>
+      <Card
+      bgImage='./images/product-and-services/product-and-services-bg-1.png'>
+
+      </Card>
+    </CardsContainer>
+    
     </div>
   );
 }
