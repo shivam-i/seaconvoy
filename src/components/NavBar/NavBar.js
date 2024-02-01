e79a317874620893a9a57894bd6c6fce47a811aa
Aniket-kr1030 <88719269+Aniket-kr1030@users.noreply.github.com>
2024-02-01 19:00:57 +0530

    Nav now responsive

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './nav-bar.css';
 
 function NavBar() {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuVisible(!isMenuVisible);
+  };
+
   return (
     <nav className="nav-bar">
       <img className="nav-bar-logo" src="./images/logo.png" alt="website-logo" />
-      <ul className="nav-links">
+      <button className="hamburger" onClick={toggleMenu}>
+        ☰
+      </button>
+      <ul className={`nav-links ${isMenuVisible ? 'visible' : ''}`}>
         <li><a href="/">Home</a></li>
         <li><a href="/">Tools</a></li>
         <li><a href="/">About</a></li>
