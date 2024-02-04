47b11e5acb444cb9e63120bfdbc88ef2539d9bc9
Shivam Singh <shivam.singh102201@gmail.com>
2024-02-04 16:35:59 +0530

    latest

--- src/components/HerosSection/HerosSection.js
+++ src/components/HerosSection/HerosSection.js
@@ -5,7 +5,9 @@ import './HerosSection.css';
 function HerosSection(props) {
   return (
     <div className="heros-section">
-      <img className="heros-section-bg-pic" src={props.imgUrl} alt="" />
+      <div className="heros-section-bg-container">
+        <img className="heros-section-bg-pic" src={props.imgUrl} alt="" />
+      </div>
       <NavBar />
       <img className="heros-section-title" src={props.titleUrl} alt="Title" />
       <img className='scroll-down' src="./images/about-page/heros-section/scroll-down.png" alt="" />
--- src/components/Home/AboutUs/AboutUs.js
+++ src/components/Home/AboutUs/AboutUs.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import Card from '../../Shared/Card'; 
-import CardsContainer from '../../Shared/CardsContainer'; 
+import Card from '../../Shared/Card';
+import CardsContainer from '../../Shared/CardsContainer';
 
 function ExampleComponent() {
   return (
     <CardsContainer>
 
       <Card
-       bgImage="./images/home-page/about-us/about-us-bg.png"
+        bgImage="./images/home-page/about-us/about-us-bg.png"
       ></Card>
       <Card
         heading="Experts in marine technology, engineering, and consultation"
       >
         <p>The team members' exposure at IIT Madras for a decade in the fields of hydrodynamic testing, validation of
-        designs, consulting for shipyards and boat builders, design approvals, and product development related to
-        defense application for powering of amphibious vehicles brings in invaluable comprehensive professional
-        resource.</p>
+          designs, consulting for shipyards and boat builders, design approvals, and product development related to
+          defense application for powering of amphibious vehicles brings in invaluable comprehensive professional
+          resource.</p>
         <a href="#learnmore">Learn More</a>
       </Card>
     </CardsContainer>
